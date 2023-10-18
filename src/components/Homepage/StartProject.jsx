945d174573ddfe4c44a53ac878b0caded4af3214
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2023-10-18 04:28:14 -0700

    CUSTOM FONTS SET UP
    CUSTOM FONTS SET UP

---
+++ src/components/Homepage/StartProject.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StartProject = () => {
+  return (
+    <div className="text-green">
+      <h1>I am a girl</h1>
+    </div>
+  );
+};
+
+export default StartProject;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Black: ["EncodeSans-Black", "sans-serif"],
+        Bold: ["EncodeSans-Bold", "sans-serif"],
+        ExtraBold: ["EncodeSans-ExtraBold", "sans-serif"],
+        ExtraLight: ["EncodeSans-ExtraLight", "sans-serif"],
+        Light: ["EncodeSans-Light", "sans-serif"],
+        Medium: ["EncodeSans-Medium", "sans-serif"],
+        Regular: ["EncodeSans-Regular", "sans-serif"],
+        SemiBold: ["EncodeSans-SemiBold", "sans-serif"],
+        Thin: ["EncodeSans-Thin", "sans-serif"],
+        Light: ["Montserrat-Light", "sans-serif"],
+        MontRegular: ["Montserrat-Regular", "sans-serif"],
+        MontBold: ["Montserrat-Bold", "sans-serif"],
+        MontSemiBold: ["Montserrat-SemiBold", "sans-serif"],
+        MontMedium: ["Montserrat-Medium", "sans-serif"],
+      },
+    },
   },
+
   plugins: [],
 };
