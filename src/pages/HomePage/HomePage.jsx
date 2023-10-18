a75a90856067a05f5413fb42ff88acbb039ff536
Okunola Mary Oluwabunmi <123972532+Ewabunmi@users.noreply.github.com>
2023-10-18 14:19:56 -0700

    Update Footer Routing
    Update Footer Routing

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import NavBar from "../../components/Homepage/NavBar";
+import Footer from "../../components/Homepage/Footer";
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
+      <Footer />
     </div>
   );
 };
