00d08a104f511eed53dbcedaaa2dbc2bf5a6de37
oddoye <joshoddoye@gmail.com>
2023-10-25 13:12:46 +0000

    Update to FindUs Page
    Update to FindUs Page. Changes made to the incorrect white bg.

---
+++ src/pages/FindUsPage/FindUsPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../../components/Homepage/NavBar'
+import FindUs from '../../components/FindUs/FindUs'
+import Footer from '../../components/Homepage/Footer'
+
+const FindUsPage = () => {
+  return (
+    <div><NavBar/>
+      <FindUs/>
+      <Footer/>
+      </div>
+  )
+};
+
+export default FindUsPage;
