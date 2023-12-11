d6ebebcc7919a6cfccb4cf3d576360907c71ded5
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2023-12-11 16:28:21 +0000

    Fixed hero text

--- src/pages/StartAProject/StartAProject.jsx
+++ src/pages/StartAProject/StartAProject.jsx
@@ -6,13 +6,13 @@ import Footer from "../../components/Homepage/Footer"
     <div>
        <NavBar />
 
-       <section className="bg-cover bg-no-repeat bg-black bg-[url('/src/assets/images/bg-solutions.png')] rounded-none lg:h-[25rem] md:h-[20rem] sm:h-[20rem] h-[20rem]">
+       <section className="bg-cover bg-no-repeat bg-black bg-[url('/src/assets/images/bg-solutions.png')] rounded-none lg:h-[20rem] md:h-[20rem] sm:h-[20rem] h-[20rem]">
          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
          <p className="text-center font-EncodeLight text-2xl text-[#2EFFE1] p-5">
            Start A Project
          </p>
          <p className="leading-5 text-white tracking-wider font-EncodeBold text-center lg:text-5xl md:text-4xl sm:text-3xl text-3xl lg:px-32 md:px-32 sm:px-24 px-4">
-            Fast-Track Success with Tailored Tech & Product Scaling Solutions
+            Start A Project With Us
           </p>
         </div>    
       </section>
