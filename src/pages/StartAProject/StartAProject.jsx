c75852a2d5b05b8886b392d3f543b9895d40e99d
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2023-12-13 11:24:46 +0000

    fixed text issue on start a project page

--- src/pages/StartAProject/StartAProject.jsx
+++ src/pages/StartAProject/StartAProject.jsx
@@ -7,10 +7,10 @@ import NavBar from "../../components/Homepage/NavBar"
 
        <section className="bg-cover bg-no-repeat bg-black bg-[url('/src/assets/images/bg-solutions.png')] rounded-none lg:h-[20rem] md:h-[20rem] sm:h-[20rem] h-[20rem]">
          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-         <p className="text-center font-EncodeLight text-2xl text-[#2EFFE1] p-5">
-           Start A Project
+         <p className="text-center font-EncodeLight text-xl md:text-2xl text-[#2EFFE1] p-5">
+           Get Started
          </p>
-         <p className="leading-5 text-white tracking-wider font-EncodeBold text-center lg:text-5xl md:text-4xl sm:text-3xl text-3xl lg:px-32 md:px-32 sm:px-24 px-4">
+         <p className="leading-10 text-white tracking-wider font-EncodeBold text-center lg:text-5xl md:text-4xl sm:text-3xl text-3xl lg:px-32 md:px-32 sm:px-24 px-4">
             Start A Project With Us
           </p>
         </div>    
