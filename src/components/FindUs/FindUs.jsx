15df672f1c32ed988f28d9cd971d2c92d5acf5a1
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2023-12-19 09:49:37 +0000

    email changed

--- src/components/FindUs/FindUs.jsx
+++ src/components/FindUs/FindUs.jsx
@@ -277,7 +277,7 @@ const FindUs = () => {
             href="mailto:info@touchstacktecnologies.com"
             className="mb-6 text-lg hover:text-green-400 underline font-EncodeMedium text-gray-500 lg:text-xl dark:text-gray-400"
           >
-            brian@touchstacktechnologies.com
+            info@touchstacktechnologies.com
           </a>
         </div>
       </div>
