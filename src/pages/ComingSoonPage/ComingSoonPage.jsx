2f9e76447226448262f8c565c5eca6d4ae22c00b
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2024-03-19 12:09:03 +0000

    Fixed Font size

--- src/pages/ComingSoonPage/ComingSoonPage.jsx
+++ src/pages/ComingSoonPage/ComingSoonPage.jsx
@@ -19,14 +19,14 @@ const ComingSoonPage = () => {
             <img src={NewLogo} alt="logo" className="md:w-[115px] w-[80px]" />
           </div>
 
-          <div className="text-[#FFFFFF] text-[8px] md:text-[12px] lg:text-[14px] font-SpaceBold">
+          <div className="text-[#FFFFFF] text-[12px] md:text-[12px] lg:text-[14px] font-SpaceBold">
             🚧 website under construction
           </div>
         </div>
 
         {/* Hero Area */}
         <div className="flex flex-col items-center justify-center text-[#FFFFFF] mt-[6.5rem]">
-          <h1 className="text-gradient font-SpaceBold text-[30.71px] md:text-[46.71px] lg:text-[66.55px] text-center">
+          <h1 className="text-gradient font-SpaceBold text-[40px] md:text-[46.71px] lg:text-[66.55px] text-center">
             Unlock the Potential of AI and Engineering Collaboration
           </h1>
           <p className="font-700 text-[#FFFFFF] font-DMSansLight text-center w-12/12 md:w-9/12 lg:w-10/12 text-[14px] lg:text-[26.34px] md:text-[16.34px] mt-10">
